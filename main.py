440be6dae72d2a4758850b22ce325b81cc1685da
Josh Nichols <jnichols20091@gmail.com>
2023-03-01 03:06:44 -0600

    fuuuu

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from playwright.sync_api import sync_playwright
+import time
 
-with sync_playwright() as p:
-    browser = p.webkit.launch()
-    page = browser.
+
+class PlayInstance:
+    def __init__(self):
+        self.playwright = sync_playwright().start()
+        self.browser = self.playwright.chromium.launch(headless=False, slow_mo=50, timeout=100000000)
+        self.page = self.browser.new_page()
+
+
+    def start_browser(self):
+        self.page.goto("https://www.abbeyresidential.com/apartments/al/hoover/riverchase/contact-us")
+        self.page.click("span")
+        self.page.fill("#contact-resident-name-lead-form-1953415", "playwright")
+        time.sleep(1000)
+
+start = PlayInstance()
+start.start_browser()
