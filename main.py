92472481be2460269c18ccc42c760ae55b75f590
Josh Nichols <jnichols20091@gmail.com>
2023-03-02 02:49:25 -0600

    working it out

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select, Select
 import time
+import re
+import random
+import string
 
 
+def rantext():
+    text = re.compile(r'[^a-zA-Z0-9]')
+    text = string.ascii_lowercase
+    result_text = ''.join(random.choice(string.ascii_lowercase) for i in range(6,45))
+    return result_text
 
 
 driver = webdriver.Firefox()
@@ -15,4 +23,7 @@ ActionChains(driver).move_to_element(poploc).click(poploc).perform()
 time.sleep(2)
 fillna = driver.find_element(By.CSS_SELECTOR, "#contact-resident-name-lead-form-1953415")
 ActionChains(driver).move_to_element(fillna).click(fillna).perform()
-ActionChains(driver).send_keys("test").perform()
+ActionChains(driver).send_keys(rantext()).perform()
+poploc1 = driver.find_element(By.CSS_SELECTOR, "#contact-resident-p-g5-resident-2-lead-form-1953415")
+ActionChains(driver).move_to_element(poploc1).click(poploc1).perform()
+ActionChains(driver).send_keys(rantext()).perform()
